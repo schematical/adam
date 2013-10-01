c555f94df972caea40f06451599488d46388bff6
Matt Lea <mlea@schematical.com>
2013-10-01 16:56:47 -0500

    triangulation melting brain

--- adam/src/main/java/com/schematical/adam/AdamObject.java
+++ adam/src/main/java/com/schematical/adam/AdamObject.java
@@ -4,6 +4,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.net.wifi.ScanResult;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
  * Created by user1a on 10/1/13.
  */
 public class AdamObject {
+
     protected AdamActivityMain am;
     private final Paint paint;
     protected String id;
@@ -18,6 +20,10 @@ public class AdamObject {
     protected int goalY = 0;
     protected int currX = 0;
     protected int currY = 0;
+
+    protected double lat;
+    protected double lng;
+    protected double altutide;
 
     protected List<AdamPing> pings;
     AdamObject(AdamActivityMain nAm, String id){
@@ -47,28 +53,66 @@ public class AdamObject {
         goalY = nGoalY;
     }
     public double GetLat(){
-        return 0.1;
+        return lat;
     }
     public double GetLng(){
-        return 0.1;
+        return lng;
     }
     public double GetAltitude(){
-        return 0.1;
+        return altutide;
     }
     public void Update(Object data){
+        java.util.Date date= new java.util.Date();
         if(data instanceof ScanResult){
             //Track in a list of pings
             ScanResult sr = (ScanResult) data;
+
             AdamPing ap = new AdamPing(
                     sr.level,
                     am.GetLocation().getLatitude(),
                     am.GetLocation().getLongitude(),
                     am.GetLocation().getAltitude(),
-                    am.GetLocation().getAccuracy()
-
-
+                    am.GetLocation().getAccuracy(),
+                    new Timestamp(date.getTime())
             );
             pings.add(ap);
         }
     }
+    public void UpdateLatLng(){
+        AdamPing p1 = null;
+        AdamPing p2 = null;
+        double a = 0;//Ping of p1
+        double b = 0;//Ping of p2
+        double c = 0;//distance between p1 and p2
+        double aAngle = 0;
+        double bAngle = 0;
+        
+        for(int i = 0; i < pings.size(); i++){
+            p2 = p1;
+            p1 = pings.get(i);
+            if(p1 != null){
+                a = p1.pingStrength * AdamPing.METERS_DMB;
+                b = p2.pingStrength * AdamPing.METERS_DMB;
+                c = p1.DistanceFrom(p2);//In Meters
+                aAngle = Math.acos(
+                        Math.pow(a,2) + Math.pow(b,2) + Math.pow(c,2) /
+                        -2 * b * c
+                );
+
+                bAngle = Math.acos(
+                        Math.pow(b,2) + Math.pow(a,2) + Math.pow(c,2) /
+                                -2 * a * c
+                );
+                double aAngleRelGround = p1.AngleInRelationTo(p2);
+                //Generate 2 estimates
+                double c1Y = Math.sin(aAngleRelGround + aAngle) * a;
+                double c1X = Math.cos(aAngleRelGround + aAngle) * a;
+                double c2Y = Math.sin(aAngleRelGround - aAngle) * a;
+                double c2X = Math.cos(aAngleRelGround - aAngle) * a;
+            }
+
+        }
+
+    }
+
 }
--- adam/src/main/java/com/schematical/adam/AdamPing.java
+++ adam/src/main/java/com/schematical/adam/AdamPing.java
@@ -1,19 +1,51 @@
 package com.schematical.adam;
 
+import java.sql.Timestamp;
+
 /**
  * Created by user1a on 10/1/13.
  */
 public class AdamPing {
+    public static final double METERS_DMB = 3/10;
     public double pingStrength;
     public double pingLat;
     public double pingLng;
     public double pingElev;
     public double pingAccuracy;
-    AdamPing(double strength, double lat, double lng, double elev, double accuracy){
+    public Timestamp pingTimestamp;
+    AdamPing(double strength, double lat, double lng, double elev, double accuracy, Timestamp ts){
         pingStrength = strength;
         pingLat = lat;
         pingLng = lng;
         pingElev = elev;
         pingAccuracy = accuracy;
+        pingTimestamp = ts;
+    }
+    public double DistanceFrom(AdamPing p2){
+        return distFrom(
+            pingLat,
+            pingLng,
+            p2.pingLat,
+            p2.pingLng
+        );
+    }
+    public double AngleInRelationTo(AdamPing p2){
+        double o = (this.pingLat - p2.pingLat);
+        double a = (this.pingLng - p2.pingLng);
+        return Math.atan(o/a);
+    }
+    public static double distFrom(double lat1, double lng1, double lat2, double lng2) {
+        double earthRadius = 3958.75;
+        double dLat = Math.toRadians(lat2-lat1);
+        double dLng = Math.toRadians(lng2-lng1);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLng/2) * Math.sin(dLng/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double dist = earthRadius * c;
+
+        int meterConversion = 1609;
+
+        return new Float(dist * meterConversion).floatValue();
     }
 }
